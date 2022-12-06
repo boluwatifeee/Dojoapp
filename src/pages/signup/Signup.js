57406e884617b8a51ef24b7bba5a07d000576051
Boluwatife <ladeindeb@gmail.ocm>
2022-12-06 10:28:49 +0100

    increased image size

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -30,8 +30,8 @@ export default function Signup() {
       setThumbnailError('Selected file must be an image')
       return
     }
-    if (selected.size > 100000) {
-      setThumbnailError('Image file size must be less than 100kb')
+    if (selected.size > 300000) {
+      setThumbnailError('Image file size must be less than 300kb')
       return
     }
     
